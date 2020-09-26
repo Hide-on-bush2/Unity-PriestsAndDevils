777bba95f2eeab75a03953f3325d8f937fb941d9
faker <skt.faker.ccj@gmail.com>
2020-09-26 18:48:24 +0800

    faker

---
+++ Assets/script/FirstController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstController : MonoBehaviour, ISceneController, IUserAction
+{
+    void Awake()
+    {
+        SSDirector director = SSDirector.getInstance();
+        director.setFPS(60);
+        director.currentSceneController = this;
+        director.currentSceneController.LoadResources();
+    }
+
+    public void LoadResources()
+    {
+        GameObject faker = Instantiate<GameObject>(
+            Resources.Load<GameObject>("prefabs/faker"),
+            Vector3.zero, Quaternion.identity);
+        faker.name = "faker";
+        Debug.Log("load faker ...  \n");
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("GameOver\n");
+    }
+
+    public void Pause()
+    {
+        
+    }
+
+    public void Resume()
+    {
+
+    }
+}
---
+++ Assets/script/UserGUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour
+{
+    private IUserAction action;
+    public void Start()
+    {
+        action = SSDirector.getInstance().currentSceneController as IUserAction;
+    }
+    public void OnGUI()
+    {
+        float width = Screen.width / 6;
+        float height = Screen.height / 12;
+        if(GUI.Button(new Rect(0, 0, width, height), "Game Over!"))
+        {
+            action.GameOver();
+        }
+    }
+}
